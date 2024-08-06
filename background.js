98eac4f1fe005a9804509fea7277aad23e29d046
Dachi Wolf <78776000+SexyWerewolf@users.noreply.github.com>
2024-08-06 05:51:23 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,22 +1,27 @@
 function delayRequest(details) {
-  return browser.storage.local.get({ rules: [] }).then((data) => {
+  return browser.storage.local.get({ enabled: true, rules: [] }).then((data) => {
+    if (!data.enabled) {
+      return { cancel: false };
+    }
     for (let rule of data.rules) {
       if (new RegExp(rule.urlPattern).test(details.url)) {
-        console.log(`${rule.urlPattern} Pattern Matched, Delaying request to: ${details.url} for ${rule.delayTime}ms`);
+        let minDelay = rule.minDelayTime;
+        let maxDelay = rule.maxDelayTime;
+        let randomDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+        console.log(`${rule.urlPattern} Pattern Matched, Delaying request to: ${details.url} for ${randomDelay}ms`);
         return new Promise((resolve) => {
           setTimeout(() => {
             console.log(`Proceeding request to: ${details.url}`);
             resolve({ cancel: false });
-          }, rule.delayTime);
+          }, randomDelay);
         });
       }
     }
     return { cancel: false };
   });
 }
-
 browser.webRequest.onBeforeRequest.addListener(
-    delayRequest,
-    {urls: ["<all_urls>"]},
-    ["blocking"]
+  delayRequest,
+  { urls: ["<all_urls>"] },
+  ["blocking"]
 );
